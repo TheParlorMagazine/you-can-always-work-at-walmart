b6c44e5005494e5bf2f3570ab2d5e29c27cf5da6
TheParlorMagazine <lindsey@theparlormagazine.com>
2025-11-12 16:54:39 +0100

    Create vite.config.ts

---
+++ src/src/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/you-can-always-work-at-walmart/' // <= IMPORTANT for GitHub Pages
+})
