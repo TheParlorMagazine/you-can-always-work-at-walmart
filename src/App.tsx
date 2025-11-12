51bfb7eb41154dd73611a96f29f60de87188e57c
TheParlorMagazine <lindsey@theparlormagazine.com>
2025-11-12 16:52:11 +0100

    Add App component with AutomationMetricsCarousel

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import AutomationMetricsCarousel from './components/AutomationMetricsCarousel'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-6">
+      <AutomationMetricsCarousel />
+    </div>
+  )
+}
